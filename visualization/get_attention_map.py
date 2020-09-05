a310d39668e79c0620ec3a0f666f29ae6a5dac45
Sungman-Cho <pccho9108@gmail.com>
2020-09-05 16:05:34 +0900

    update some codes

---
+++ visualization/get_attention_map.py
@@ -0,0 +1,15 @@
+### reference : https://discuss.pytorch.org/t/visualize-feature-map/29597
+
+### import libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import torchvision.transforms as transforms
+import torhcvision.datasets as datasets
+
+import matplotlib.pyplot as plt
+
+class 
